185bfd563791839cf4c8f7b4de8015d6ede86cc2
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-12-18 15:54:57 +0300

    refactor(Input): стилизация; эффекты наведения

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,32 +1,31 @@
 import styles from './Input.module.css'
-import type { InputProps } from "../../../types/components/Input/input-props"
+import type { InputProps } from '../../../types/components/Input/input-props'
 import { Flex } from '../../layout/Flex/Flex'
 
 export const Input = ({
-  type = 'text',
-  placeholder,
-  disabled = false,
-  required = false,
-  error = false,
-  inputSize = 'small',
-  icon,
-  ...props
-}: InputProps) => {
-  return (
-    <Flex align='center' className={styles.inputWrapper}>
-      {icon && <span className={styles.icon}>{icon}</span>}
-      <input
-        {...props}
-        type={type}
-        placeholder={placeholder}
-        disabled={disabled}
-        required={required}
-        className={`
-          ${styles.input}
-          ${error ? styles.error : ''}
-          ${styles[inputSize]}
-          ${icon && styles.inputWithIconPadding}`}
-      />
-    </Flex>
-  )
+                          type = 'text',
+                          placeholder,
+                          disabled = false,
+                          required = false,
+                          error = false,
+                          inputSize = 'small',
+                          icon,
+                          ...props
+                      }: InputProps) => {
+    return (
+        <Flex
+            align="center"
+            className={`${styles.inputWrapper} ${styles[inputSize]} ${disabled ? styles.disabled : ''} ${error ? styles.error : ''}`}
+        >
+            {icon && <span className={styles.icon}>{icon}</span>}
+            <input
+                {...props}
+                type={type}
+                placeholder={placeholder}
+                disabled={disabled}
+                required={required}
+                className={`${styles.input} ${icon ? styles.withIcon : ''}`}
+            />
+        </Flex>
+    )
 }

139b0c50e4d118c1f85c7be4428e1cd950b1abf5
Zapolyarny <zapolyarnynorth@gmail.com>
2025-12-19 08:11:10 +0300

    feat(pages): шапка на странице авторизации

--- frontend/src/pages/Auth/AuthPage.tsx
+++ frontend/src/pages/Auth/AuthPage.tsx
@@ -2,10 +2,12 @@ import {AuthForm} from "../../components/organisms/AuthForm/AuthForm.tsx";
 import {PageWrapper} from "../../components/layout/PageWrapper/PageWrapper.tsx";
 import {Flex} from "../../components/layout/Flex/Flex.tsx";
 import styles from './AuthPage.module.css'
+import {Header} from "../../components/organisms/Header/Header.tsx";
 
 function AuthPage() {
     return (
         <PageWrapper>
+            <Header isAuth={false} hideAccountControls={true} />
             <Flex justify="center" className={styles.formWrapper}>
                 <AuthForm />
             </Flex>
